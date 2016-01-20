c2b7098c5826669298f2dc48810b09a4e932c31c
Lindsey <lindsey.raffa@gmail.com>
2016-01-19 16:10:17 -0800

    all the things are working

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,44 @@ var leapYear = function(year){
   var yearFetched = parseInt(year);
 
   if ((yearFetched < 0) || isNaN(yearFetched)){
-    return true;
+    alert("Please enter a valid number");
+    return false;
   } else if ((yearFetched % 4 == 0) && (yearFetched % 100 !== 0) || (yearFetched % 400 == 0)){
       return true;
   } else {
-    return false;
+    $(".not").text("not");
+    $("#result").show();
+    return true;
   }
 };
 
 var checkEmpty = function(year){
     if(!year){
+    alert("You fool! You entered nothing. Try again!");
+    $('#result').hide();
     return true;
   } else {
     return false;
   }
 };
+
+
+$(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = $('#year').val();
+    var emptyChecked = checkEmpty(year);
+
+    checkEmpty(year);
+
+    if(!emptyChecked){
+       if(!leapYear(year)){
+         $('#result').hide();
+         return false;
+        } else {
+          $(".year").text(year);
+          $("#result").show();
+          }
+        }
+    event.preventDefault();
+  });
+});
